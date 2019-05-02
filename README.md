b9388ca7db677b813022bca468e5ed256af62472
Jack W <jack.wampler@colorado.edu>
2019-05-02 12:33:24 -0600

    working split of MNIST

---
+++ README.md
@@ -0,0 +1,55 @@
+# Basic MNIST Example
+
+~Questions~:
+
+* Can we fragment a model and give a piece to the client such that they encode their data in
+a non-trivial way specifically relevant to the task that a service provider wishes to perform on that
+data?
+* How hard would it be for a service provider to re-create the original sample?  
+
+This example makes use of the mnist classification task to demonstrate the ability to split a model
+and the efficacy with which a service provider can reconstruct original samples.
+
+## Install and Run example
+
+```bash
+pip install -r requirements.txt
+python main.py
+# CUDA_VISIBLE_DEVICES=2 python main.py  # to specify GPU id to ex. 2
+```
+
+## Split model
+
+After training, the model is split such that the weights of the frontend piece can be transmitted to
+the client, and the secondary piece then operates on the generated intermediary.
+
+This works naturally as there is no change in the weights or the dimensionality of the model.
+
+## Reverse Model
+
+As we rely on the intermediary representation to provide some measure of obfuscation we next test
+the reliability with which the model can be reversed by the service provider to re-create the original
+sample.
+
+This is done simply as we have both the orioginal as a label and the generated intermediary to test against.
+
+## Discussion
+
+### Benefits
+
+Clients don't receive the entire model so there is still some ides of a proprietary model.
+
+We offload some of the computation to the client so that they can gain this benefit.
+
+
+
+## Question for future work
+
+* How do model complexity and the distance through the model where the split is inserted affect:
+    - the amount of computational work a client has to do
+    - the ability of a service to re-create the origianl sample
+    - the compression factor of the sample for network transfer
+    - the size of required model weight initializations transfered to the client
+
+* Can we construct the client side model fragment to be transferable to related classification tasks
+without directly revealing information about the client?
---
+++ main.py
@@ -0,0 +1,166 @@
+from __future__ import print_function
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+    def diff_forward(self, x):
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+    def split(self):
+        return (self.state_dict()['conv1.weight'], self.state_dict()['conv1.bias'])
+
+class CNet(nn.Module):
+    def __init__(self, weight, bias):
+        super(CNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv1.weight = nn.Parameter(weight)
+        self.conv1.bias = nn.Parameter(bias)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        return x
+    
+def train(args, model, device, train_loader, optimizer, epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % args.log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+
+def test(args, c_model, model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            
+            client_out = c_model.forward(data)
+
+            output = model.diff_forward(client_out)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+    return test_loss, client_out.shape
+
+def graph():
+    pass
+
+
+
+
+def main():
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    
+    parser.add_argument('--save-model', action='store_true', default=False,
+                        help='For Saving the current Model')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(args.seed)
+
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=True, download=True,
+                       transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST('./data', train=False, transform=transforms.Compose([
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.1307,), (0.3081,))
+                       ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+
+
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+
+    epoch_mse=[]
+    epoch_d_size=[]
+    reverse={}
+    reverse['interms']=[]
+    reverse['originals']=[]
+    
+
+    for epoch in range(1, args.epochs + 1):
+        train(args, model, device, train_loader, optimizer, epoch)
+        w1, b1 = model.split()
+        c_model = CNet( w1, b1 ).to(device)
+        mse, p_size = test(args, c_model, model, device, test_loader)
+
+    if (args.save_model):
+        torch.save(model.state_dict(),"mnist_cnn.pt")
+
+    print(model.state_dict()['conv1.weight'].shape)
+    print(model.state_dict()['conv1.bias'].shape)
+    # for key, value in model.state_dict().items():
+    #     print()
+        
+if __name__ == '__main__':
+    main()
